19e963cb22a4886e7ae72f1dc0eb4e3c58b3164a
Ujjwal Kumar <19089@HINDUJA-TECH.COM>
2024-09-13 16:14:53 +0530

    new chnages

--- vite.config.js
+++ vite.config.js
@@ -26,6 +26,6 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    historyApiFallback: true, 
+    historyApiFallback: true, // Correctly allows refreshing to work for SPA.
   },
 });
